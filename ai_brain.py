3b315852222fa817c1ee72e58d958b7e7b41dfa6
gnani291 <gnanitadiparthi@gmail.com>
2026-01-27 09:44:55 +0530

    Clean up ai_brain.py by removing blank lines
    Removed unnecessary blank lines for cleaner code.

--- ai_brain.py
+++ ai_brain.py
@@ -1,14 +1,10 @@
 import os
 from dotenv import load_dotenv
 from groq import Groq
-
 load_dotenv()
-
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 MODEL_NAME = os.getenv("MODEL_NAME")
-
 client = Groq(api_key=GROQ_API_KEY)
-
 def get_ai_response(user_query):
 
     try:
