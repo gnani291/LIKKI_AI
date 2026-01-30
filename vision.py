212f2ec1a6d3defa4541a36e31c5cabe32931e46
gnani291 <gnanitadiparthi@gmail.com>
2026-01-30 18:16:23 +0530

    Remove unnecessary newline in vision.py

--- vision.py
+++ vision.py
@@ -2,7 +2,6 @@ import pytesseract
 import cv2
 import numpy as np
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-
 def extract_text(image_path):
     img = cv2.imread(image_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
