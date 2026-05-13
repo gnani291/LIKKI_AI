018af10161378572afcc8d54245b6b86b8d479a2
Tadiparthi Gnaneswar <gnanitadiparthi@gmail.com>
2026-05-13 22:35:56 +0530

    Add import for dotenv and load environment variables

--- ai_brain.py
+++ ai_brain.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from groq import Groq
 load_dotenv()
+
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 MODEL_NAME = os.getenv("MODEL_NAME")
 client = Groq(api_key=GROQ_API_KEY)
