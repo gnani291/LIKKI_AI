d6851243942434e99968100b89e835f0a62aafd4
Tadiparthi Gnaneswar <gnanitadiparthi@gmail.com>
2026-05-14 20:19:03 +0530

    Remove unnecessary blank line in main.py

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from groq import Groq
 from datetime import datetime
 from tts_output import speak  
 
-
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 MODEL_NAME = os.getenv("MODEL_NAME")
